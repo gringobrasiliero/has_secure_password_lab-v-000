13ce70df1bc0bcd538f1b143a429f413cacd6eb6
Nolan Byrnes <nolanehs@gmail.com>
2018-03-17 12:19:19 +0000

    automatically backed up by learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+def create
+  @user = User.find_by(name: params[:name])
+  require_pass
+  session[:user_id] = @user.id
+end
+
+
+private
+def def require_pass
+  return head(:forbidden) unless @user.authenticate(params[:password])
+end
+
+end
